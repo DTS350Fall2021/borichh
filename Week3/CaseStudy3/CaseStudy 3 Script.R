a1daa9adde3354f387d1e67110022fc4fe8e3e5c
haleighborich <borichh.19@william.jewell.edu>
2021-09-13 21:16:15 -0500

    Case Study 3 Completed

--- Week3/CaseStudy3/CaseStudy 3 Script.R
+++ Week3/CaseStudy3/CaseStudy 3 Script.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 install.packages("gapminder")
 library(gapminder)
+library(dplyr)
+library(ggplot2)
 
 view(gapminder)
 
@@ -11,10 +13,20 @@ ggplot(data = WealthLife) +
   geom_point(mapping = aes(x = lifeExp, y = gdpPercap, color = continent, size = pop)) +
   facet_wrap(~ year, nrow = 1) +
   scale_y_continuous(trans = "sqrt") +
+  xlab("Life Expectancy") + ylab("GDP Per Capita") +
   theme_bw()
 
-weightedavg <- weighted.mean(WealthLife, gdpPercap)
+WeightedAvg <- WealthLife %>%
+  group_by(year, continent) %>%
+  summarise(average = weighted.mean(gdpPercap), population = pop/10000)
+WeightedAvg
 
-ggplot(data = WealthLife) +
-  geom_point(mapping = aes(x = year, y = gdpPercap, color = continent)) +
-  facet_wrap(~ continent, nrow = 1)
+ggplot(data = WealthLife, mapping = aes(x = year, y = gdpPercap)) +
+  geom_point(data = WealthLife, mapping = aes(color = continent)) +
+  geom_line(data = WealthLife, mapping = aes(color = continent, group = country)) +
+  geom_point(data = WeightedAvg, aes(x = year, y = average, size = population)) +
+  geom_line(data = WeightedAvg, aes(x = year, y = average)) +
+  facet_wrap(~ continent, nrow = 1) +
+  xlab("Year") + ylab("GDP Per Capita") +
+  scale_size_continuous(name = "Population (100k)", breaks = c(10000, 20000, 30000)) +
+  theme_bw()
